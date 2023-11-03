a58b90b2bbfb49419fa13564cd31248d710f9cd1
Supermjork <markehabaziz@gmail.com>
2023-11-03 10:28:25 +0200

    Tasks updated so far

---
+++ task_1/app/src/main/java/com/professional/task1/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.professional.task1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btn = (Button)findViewById(R.id.prev_activity);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            }
+        });
+    }
+}
